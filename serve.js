72b69ed57214d48be29336e9811ecaf29dae3622
mpsido <1979371837@qq.com>
2019-08-09 12:44:52 +0800

    Ask front end to display the environment he knows of at startup

--- serve.js
+++ serve.js
@@ -10,6 +10,10 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
+console.log("REACT_APP_RELAYER_URL:", process.env.REACT_APP_RELAYER_URL);
+console.log("REACT_APP_DEFAULT_BASE_PATH:", process.env.REACT_APP_DEFAULT_BASE_PATH);
+console.log("REACT_APP_THEME_NAME:", process.env.REACT_APP_THEME_NAME);
+console.log("REACT_APP_NETWORK_ID:", process.env.REACT_APP_NETWORK_ID);
 https.createServer(
     {
         key: fs.readFileSync('encryption/private.key'),
